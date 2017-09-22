de9660521aa8984e3365f3d3792b001515ddc700
SURANRA <raju.gangadhar.suranagi@one.verizon.com>
2017-09-22 20:28:44 +0530

    Adding animated rendering

--- src/components/CircularProgressBar.js
+++ src/components/CircularProgressBar.js
@@ -4,7 +4,16 @@ export default class CircularProgressBar extends Component {
 
     constructor(props){
         super(props);
-        this.state = { percentage: this.props.percentage }
+        this.state = { currPerc: 0, percentage: this.props.percentage }
+    }
+
+    componentDidMount(){
+        /*while(this.state.currPerc < this.state.percentage){
+            setTimeout(()=>{
+                let currPerc = this.state.currPerc+1;
+                this.setState({currPerc})
+            }, 200);
+        }*/
     }
 
     render(){
