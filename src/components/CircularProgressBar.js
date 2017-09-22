fb8b1a79a3c3b7bd050b0854d4e4d76e34e701c5
Raju Gangadhar <geetagangadharraju@gmail.com>
2017-09-23 01:59:47 +0530

    Final changes

--- src/components/CircularProgressBar.js
+++ src/components/CircularProgressBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
+const fastTimeout = 5, slowTimeout = 30;
 export default class CircularProgressBar extends Component {
+
 
     constructor(props){
         super(props);
@@ -8,25 +10,41 @@ export default class CircularProgressBar extends Component {
     }
 
     componentDidMount(){
-        /*while(this.state.currPerc < this.state.percentage){
-            setTimeout(()=>{
-                let currPerc = this.state.currPerc+1;
-                this.setState({currPerc})
-            }, 200);
-        }*/
+        setTimeout(()=>this.setState({currPerc: this.state.currPerc+1}), 30);
     }
 
     render(){
         const color = `${this.props.iconColor}`;
         return (
-            <div style={{color}}>
-                Progress bar here
-                <div>
-                    <span className={this.props.iconClass} ></span><br/>
-                    <span className="perc-text">{this.state.percentage}%</span>
+            <div>
+                <div className="outer-ring" style={{border: `1px solid ${color}`}}>
+                    <div style={{color: color, border: `10px solid ${color}`, margin:'2px'}} className='circle-bar'>
+                        <div className='inset-data'>
+                            <span className={this.props.iconClass} ></span><br/>
+                            <span className='perc-text'>
+                                <span style={{fontSize:'11px'}}>+</span>
+                                <span style={{fontWeight:'bold',fontSize:'16px',padding:'3px'}}>{this.state.currPerc}</span>
+                                <span style={{fontSize:'11px'}}>%</span>
+                            </span>
+                        </div>
+                    </div>
                 </div>
+                <div className="label-text">{this.props.labelText}</div>
             </div>
         );
+    }
+
+    componentDidUpdate(){
+        let { currPerc, percentage } = this.state;
+        if(currPerc < percentage){
+            if(currPerc > (percentage - 0.15*percentage )) {
+                setTimeout(() => this.setState({currPerc: currPerc + 1}), slowTimeout);
+            }else{
+                setTimeout(() => this.setState({currPerc: currPerc + 1}), fastTimeout);
+            }
+        }else{
+            console.log(Date.now());
+        }
     }
 }
 
