f8a5666b2595972a84dc58a922b858580b352ae4
SURANRA <raju.gangadhar.suranagi@one.verizon.com>
2017-09-22 20:27:54 +0530

    Re-constructed. Added default props

--- src/components/app.js
+++ src/components/app.js
@@ -6,28 +6,44 @@ class App extends Component {
 
     constructor(props){
         super(props);
-
     }
+
+    /**
+     * Generate as many progress bars as passed to this component
+     * @returns {Array} of JSX tags to show circular progress bars
+     */
+    getProgressBars(){
+
+        let { progressBars } = this.props;
+        if(progressBars == null
+            || !Array.isArray(progressBars)
+            || progressBars.length == 0 ){
+            return ;
+        }
+        return progressBars.map((bar,index)=>{
+            return (
+                <div className="col-md-4 progressbar">
+                    <CircularProgressBar key={index} percentage={bar.perc} iconClass={bar.iconClass} iconColor={bar.iconColor}/>
+                </div>
+            );
+        });
+    }
+
     render(){
-
-        const user = 'glyphicon glyphicon-user';
-        const thumbsUp = 'glyphicon glyphicon-thumbs-up';
-        const horn = 'glyphicon glyphicon-bullhorn';
-
         return (
             <div className="row">
-                <div className="col-md-4 progressbar">
-                    <CircularProgressBar percentage="40" iconClass={thumbsUp} iconColor="red"/>
-                </div>
-                <div className="col-md-4 progressbar">
-                    <CircularProgressBar percentage="60" iconClass={horn} iconColor="cyan"/>
-                </div>
-                <div className="col-md-4 progressbar">
-                    <CircularProgressBar percentage="30" iconClass={user} iconColor="yellow"/>
-                </div>
+                {this.getProgressBars()}
             </div>
-        )
+        );
     }
+}
+
+App.defaultProps = {
+    progressBars: [
+        {perc: 50, iconClass: 'glyphicon glyphicon-thumbs-up', iconColor:"red"},
+        {perc: 27, iconClass: 'glyphicon glyphicon-bullhorn', iconColor:"cyan"},
+        {perc: 76, iconClass: 'glyphicon glyphicon-user', iconColor:"yellow"}
+    ]
 }
 
 export default App;
